e3526276493980b07e68dad11c704ae9461ceac7
B <benricheson101@gmail.com>
2020-07-02 08:45:59 -0400

    aaaa

--- aaaaa.scm
+++
@@ -1,8 +0,0 @@
-(define-macro for
-  (lambda (iterator start end . body)
-    `(let loop ((,iterator ,start))
-      (if (<= ,iterator ,end)
-      (loop (+ ,iterator 1) (begin ,@body))))))
-
-(for x 1 10000000000000000000000000000
-  (print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
