13bdb8dee037ff0293e3d40e89e25c9e3f9a94e9
Wumpus <67572166+Pikachunou@users.noreply.github.com>
2020-08-02 23:43:12 -0700

    Add files via upload

---
+++ ok.pl
@@ -0,0 +1,10 @@
+#!C:\strawberry\perl\bin\perl.exe
+#
+# The traditional first program.
+ 
+# Strict and warnings are recommended.
+use strict;
+use warnings;
+ 
+# Print a message.
+print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
