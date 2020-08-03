2c6e416c1b796d038ef46b62e5d2af63cb81d9c3
Wumpus <67572166+Pikachunou@users.noreply.github.com>
2020-08-02 23:46:31 -0700

    Update aaa.pl
    aaaa

--- aaa.pl
+++ aaa.pl
@@ -1,10 +1,6 @@
 #!C:\strawberry\perl\bin\perl.exe
-#
-# The traditional first program.
  
-# Strict and warnings are recommended.
 use strict;
 use warnings;
  
-# Print a message.
 print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
