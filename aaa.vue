64e9e8f08413f19348cbea662f59e7d18be0dd39
Khaleel Gibran <khalby786@gmail.com>
2020-10-16 08:50:12 +0400

    [Hacktoberfest] .vue file

---
+++ aaa.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{aaa}}</p>
+</template>
+
+<script>
+  
+  export default {
+    name: 'Aaa',
+    data: function() {
+      return {
+        aaa: ""
+      }
+    },
+    created: function() {
+      setInterval(() => this.aaa += "a", 1)
+    }
+  }
+  
+</script>
