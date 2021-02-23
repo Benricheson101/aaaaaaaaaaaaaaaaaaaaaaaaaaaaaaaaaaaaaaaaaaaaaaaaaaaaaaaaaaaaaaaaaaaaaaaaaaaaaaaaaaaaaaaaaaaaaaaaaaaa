0cba6f32ad4425051627054285e0b3aa527864f3
B <benricheson101@gmail.com>
2021-02-23 18:16:45 -0500

    a

--- aaa.c
+++ aaa.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main () {
-  while (1) {
-    printf("a");
-  }
-}
+#define a int
+#define aa main
+#define aaa (
+#define aaaa )
+#define aaaaa {
+#define aaaaaa while
+#define aaaaaaa 1
+#define aaaaaaaa printf
+#define aaaaaaaaa "%s"
+#define aaaaaaaaaa ,
+#define aaaaaaaaaaa "a"
+#define aaaaaaaaaaaa ;
+#define aaaaaaaaaaaaa return
+#define aaaaaaaaaaaaaa 0
+#define aaaaaaaaaaaaaaa }
+
+a aa aaa aaaa aaaaa
+  aaaaaa aaa aaaaaaa aaaa aaaaa
+    aaaaaaaa aaa aaaaaaaaa aaaaaaaaaa aaaaaaaaaaa aaaa aaaaaaaaaaaa
+  aaaaaaaaaaaaaaa
+  aaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaa
+aaaaaaaaaaaaaaa
